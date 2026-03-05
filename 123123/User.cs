80e726616f85e19f451a0f440641dc918d9363d5
rasmus <rsla79145@edu.ucl.dk>
2026-03-05 22:44:23 +0100

    fields til user og userService klassen

---
+++ 123123/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _123123
+{
+    internal class User
+    {
+        public string _UserName { get; private set; }
+        public string _Password { get; private set; }
+        public int _UserID { get; private set; }
+
+        public User(string Username, int UserID, string Password)
+        {
+            _UserName = Username;
+            _Password = Password;
+            _UserID = UserID;
+
+
+
+
+        }
+    }
+}
---
+++ 123123/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _123123
+{
+    internal class UserService
+    {
+
+        public void RegisterBruger(Bruger Username)
+        {
+            Console.WriteLine("===============================================".PadLeft(50));
+            Console.WriteLine("               REGISTER BRUGER                 ".PadLeft(50));
+            Console.WriteLine("===============================================".PadLeft(50));
+
+            Console.Write("Indtast Dit brugernavn: ");
+            string Username = Console.ReadLine().ToLower();
+
+            Console.Write("Indtast Dit adgangkode: ");
+            String Password = Console.ReadLine().ToLower();
+
+            Bruger NewUser = new Bruger(Username, Password);
+
+        }
+    }
+}
