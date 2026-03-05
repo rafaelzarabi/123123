dab097fc1a9da773f4160c8cb6a279b9e972290a
Rafael Adl Zarabi <rafaelzarabi@gmail.com>
2026-03-05 14:17:45 +0100

    Add project files.

---
+++ 123123/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _123123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
